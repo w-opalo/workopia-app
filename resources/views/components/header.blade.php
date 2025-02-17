c871727827fc6ba24e24c0c3082a4d6e004fa9b9
Willice Opalo <williceopalo11@gmail.com>
2025-02-17 15:12:28 +0300

    modified the header component

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,12 @@
-{{-- <?php
-    $isActive = request()->is('jobs');
+<?php
+    // $isActive = request()->is('jobs');
 
-    echo $isActive ? 'Avtive' : '';
+    // echo $isActive ? 'Avtive' : '';
 
     // if ($isActive) {
     //     echo $isActive; // Output the variable
     // }
-?> --}}
+?>
 
 <!-- Header -->
 <header class="bg-blue-900 text-white p-4">
@@ -20,15 +20,21 @@
            
             <x-nav-link url='/jobs' :active="request()->is('jobs')">All Jobs</x-nav-link>
 
-            
             @auth
             <x-nav-link url='/jobs/saved' :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
+
             <x-nav-link url='/dashboard' :active="request()->is('dashboard')" icon='gauge'>Dashboard</x-nav-link>
+
             <x-button-link url="/jobs/create" icon='edit'> Create Job </x-button-link>
+
             <x-logout-button />
+
             @else
+
             <x-nav-link url='/login' :active="request()->is('login')">Login</x-nav-link>
+
             <x-nav-link url='/register' :active="request()->is('register')">Register</x-nav-link>
+
             @endauth
         </nav>
 
@@ -42,14 +48,23 @@
     <nav id="mobile-menu" class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
 
         <x-nav-link url='/jobs' :active="request()->is('jobs')" :mobile='true'>All Jobs</x-nav-link>
+
         @auth 
+
         <x-nav-link url='/jobs/save' :active="request()->is('jobs/save')" :mobile='true'>Saved Jobs</x-nav-link>
+
         <x-nav-link url='/dashboard' :active="request()->is('dashboard')" :mobile='true'>Dasboard</x-nav-link>
+
         <x-button-link url="/jobs/create" icon='edit' :block='true'> Create Job </x-button-link>
+
         <x-logout-button />
+
         @else    
+
         <x-nav-link url='/login' :active="request()->is('login')" :mobile='true'>Login</x-nav-link>
+
         <x-nav-link url='/register' :active="request()->is('register')" :mobile='true'>Register</x-nav-link>
+        
         @endauth
     </nav>
 </header>
